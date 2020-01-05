02c809ccb84c46f0197f8b828206985e488f530a
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2020-01-05 20:02:31 -0300

    :sparkles: Creating .vue component to add files

---
+++ src/modules/dashboard/components/AddFilesToRequest.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <v-btn color="success" @click.stop="openFileUploader">
+      <v-icon>mdi-camera-plus</v-icon>
+    </v-btn>
+    <v-file-input
+      v-show="false"
+      @click.stop
+      @change="sendFiles"
+      :id="`uploadFiles-${requestId}`"
+      small-chips
+      multiple
+      v-model="files"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    requestId: Number
+  },
+  data() {
+    return {
+      files: []
+    };
+  },
+  methods: {
+    openFileUploader() {
+      document.getElementById(`uploadFiles-${this.requestId}`).click();
+    },
+    sendFiles() {
+      const formDataFiles = new FormData();
+      for (let file of this.files) formDataFiles.append("files", file);
+      this.$store.dispatch("addFilesToRequest", {
+        requestId: this.requestId,
+        formDataFiles
+      });
+    }
+  }
+};
+</script>
