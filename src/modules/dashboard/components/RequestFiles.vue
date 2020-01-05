c27dfc5014f4b8687ee1d8413cf8bd20840e485d
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2020-01-05 20:27:27 -0300

    :sparkles: Creating component to show files related to a Request

---
+++ src/modules/dashboard/components/RequestFiles.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-if="files.length > 0">
+      {{files}}
+      <v-row>
+        <v-col cols="12" v-for="file in files" :key="file.id">
+          <v-btn @click.stop block outlined v-text="file.name" color="primary" />
+        </v-col>
+      </v-row>
+    </div>
+    <div v-else>
+      <v-btn @click.stop block outlined v-text="'No files :('" color="secondary" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    files: Array
+  }
+};
+</script>
