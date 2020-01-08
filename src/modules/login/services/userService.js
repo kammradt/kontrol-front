fc6299ee6611a98143e085b019b3d7b3be0be2fc
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2020-01-08 10:13:35 -0300

    :fire:

--- src/modules/login/services/userService.js
+++ src/modules/login/services/userService.js
@@ -52,8 +52,6 @@ export default {
     } catch (error) {
       if (error.response.status === 400)
         notyf.error(error.response.data.message);
-      if (error.response.status === 404)
-        notyf.error(error.response.data.message);
       throw 'NotAbleToGetCurrentUser'
     }
   },
