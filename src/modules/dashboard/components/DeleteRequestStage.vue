8e9b457d2e81a2a10e2cd41432cad3703589fcf0
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2020-01-14 00:01:28 -0300

    :art: Using mapActions on DeleteRequestStage and removing extra code

--- src/modules/dashboard/components/DeleteRequestStage.vue
+++ src/modules/dashboard/components/DeleteRequestStage.vue
@@ -1,19 +1,18 @@
 <template>
-  <v-btn color="closed" icon dark @click.stop="deleteRequestStage">
+  <v-btn color="closed" icon dark @click.stop="deleteRequestStage(requestStageId)">
     <v-icon v-text="'mdi-delete-empty'" />
   </v-btn>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
   name: "DeleteRequestStage",
   props: {
     requestStageId: Number
   },
   methods: {
-    deleteRequestStage() {
-      this.$store.dispatch("deleteRequestStage", this.requestStageId)
-    }
+    ...mapActions(['deleteRequestStage']),
   }
 };
 </script>
